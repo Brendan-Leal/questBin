54819eae15771225910a441fe1f7b14524f912c8
Brendan Leal <brendan.leal@outlook.com>
2022-02-02 08:35:36 -0700

    require functions and modified routes

--- app.js
+++ app.js
@@ -3,35 +3,36 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT;
 
+const parseRequest = require('./lib/javascripts/parseRequest');
+const createBin = require('./lib/javascripts/createBin');
+
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-  res.send('Hello World')
-  //Homepage
-})
+app.get('/api/create-bin', async (req, res) => {
+  await createBin();
+  res.render('inspect-bin');
 
-app.post('/', (req, res) => {
-  //Submit the form / requesting a new URL
-})
-
-
-app.get('/:id/view', (req, res) => {
-  //Getting all of the requests in the bin
-})
-
-app.post('/:id', (req, res) => {
-  //Sending a post request to a bin
-  //Capture the entire request
-})
-
-app.put('/:id', (req, res) => {
-  //Capture a put request to a bin
-})
-
-app.delete('/:id', (req, res) => {
-  //Capture a delete request to a bin
-})
+  // TODO: Decide what view engine to use
+  res.sendFile(__dirname + "/public/view.html");
+});
 
 
 
-app.listen(port, () => console.log(`Listening on port ${port}`))
+
+
+// app.post('/:id', (req, res) => {
+//   //Sending a post request to a bin
+//   //Capture the entire request
+// });
+
+// app.put('/:id', (req, res) => {
+//   //Capture a put request to a bin
+// });
+
+// app.delete('/:id', (req, res) => {
+//   //Capture a delete request to a bin
+// });
+
+
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
